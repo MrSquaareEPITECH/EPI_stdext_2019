8e71197a0f69c34197918facfe641216f33e3c32
Guillaume Bonnet <28934649+MrSquaare@users.noreply.github.com>
2020-06-13 01:26:04 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,19 +5,7 @@
 ** main.c
 */
 
-#include <stdio.h>
-#include <stringext.h>
-
 int main(void)
 {
-    char string1[] = " \t";
-    char string2[] = "Hello, World! \t";
-
-    char *result1 = strltrim(string1, "\t ");
-    char *result2 = strrtrim(string2, "\t ");
-
-    printf("result1: %s$\n", result1);
-    printf("result2: %s$\n", result2);
-
     return (0);
 }
